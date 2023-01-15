125f95761ee8f081c73d465bdc1423872cc41b6d
Lam Nhan <nhan.lh@raksul.com>
2023-01-15 22:24:23 +0700

    Update header

--- components/header.ts
+++ components/header.ts
@@ -1,6 +1,7 @@
 import {
   TiniComponent,
   Component,
+  Reactive,
   Input,
   html,
   css,
@@ -11,57 +12,122 @@ import {
 @Component('app-header')
 export class HeaderComponent extends TiniComponent {
   @Input() sticky = false;
+  @Reactive() mobileExpanded = false;
+
+  toggleMobileMenu() {
+    this.mobileExpanded = !this.mobileExpanded;
+  }
 
   protected template = html`<header class=${classMap({sticky: this.sticky})}>
-    <div class="brand">
-      <a href="/"><img src="../assets/logo-name.svg" /></a>
-    </div>
-    <div class="menu">
-      <div class="toggler">
-        <button><i class="icon-2x icon-menu"></i></button>
-      </div>
-      <nav class="nav">
-        <ul>
-          <li class="docs"><a href="/docs">Docs</a></li>
-          <li class="support"><a href="/support">Support</a></li>
-          <li class="about"><a href="/about">About</a></li>
-        </ul>
-      </nav>
-      <div class="links">
-        <ul>
-          <li class="github">
-            <a href="https://github.com/tinijs" target="_blank">
-              <i class="icon-2x icon-github"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <button class="toggler" @click=${this.toggleMobileMenu}>
+      <i
+        class=${classMap({
+          icon: true,
+          'icon-menu': !this.mobileExpanded,
+          'icon-close': this.mobileExpanded,
+        })}
+      ></i>
+    </button>
+    <a href="/" class="brand">
+      <img src="../assets/logo.svg" />
+      <h1>Tini</h1>
+    </a>
+    <ul class=${classMap({menu: true, expanded: this.mobileExpanded})}>
+      <li class="docs"><a href="/docs">Docs</a></li>
+      <li class="modules"><a href="/modules">Modules</a></li>
+      <li class="support"><a href="/support">Support</a></li>
+      <li class="about"><a href="/about">About</a></li>
+    </ul>
+    <ul class="links">
+      <li class="github">
+        <a href="https://github.com/tinijs" target="_blank">
+          <i class="icon icon-github"></i>
+        </a>
+      </li>
+    </ul>
   </header>`;
 
   static styles = [
-    unistylus``,
+    unistylus`
+      icon-menu
+      icon-close
+    `,
     css`
       header {
-        margin: 0;
+        background: gray;
+        width: 100%;
+        max-width: 1200px;
+        margin: auto;
+        box-sizing: border-box;
+        padding: 1rem 0.5rem;
+        border-bottom: 1px solid transparent;
+        display: flex;
+        align-items: center;
+        line-height: 1;
 
         &.sticky {
+          position: sticky;
+          border-color: var(--color-medium);
+        }
+      }
+
+      .toggler {
+        background: none;
+        border: 0;
+        padding: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        .icon {
+          --size: 28px;
         }
       }
 
       .brand {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-decoration: none;
+
         img {
+          width: 1.5rem;
+          margin-right: 5px;
+        }
+
+        h1 {
+          display: none;
         }
       }
 
       .menu {
-        .toggler {
-        }
+        background: var(--color-background);
+        background: #aaaaaa;
+        display: none;
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        position: fixed;
+        border-top: 1px solid transparent;
+        top: 64px;
+        left: 0;
+        width: 100%;
+        height: calc(100vh - 64px);
 
-        .nav {
+        &.expanded {
+          display: block;
+          border-color: var(--color-medium);
         }
+      }
 
-        .links {
+      .links {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+
+        .icon {
+          --size: 20px;
         }
       }
     `,
